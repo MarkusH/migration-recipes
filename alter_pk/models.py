40b666d979661787ca28b377eb61128a9f3ecbd9
Markus Holtermann <info@markusholtermann.eu>
2016-02-21 21:40:35 +1100

    Recipe #4 -- Made Author.name PK

--- alter_pk/models.py
+++ alter_pk/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50, primary_key=True)
 
 
 class Post(models.Model):
