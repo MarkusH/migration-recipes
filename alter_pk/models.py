b0e155f7fc3f1de871002a9555f4d3ad517d55cf
Markus Holtermann <info@markusholtermann.eu>
2016-02-21 23:25:58 +1100

    Recipe #4 -- Renamed Post.author_name to Post.author

--- alter_pk/models.py
+++ alter_pk/models.py
@@ -7,4 +7,4 @@ class Author(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
-    author_name = models.ForeignKey(Author, null=True)
+    author = models.ForeignKey(Author, null=True)
