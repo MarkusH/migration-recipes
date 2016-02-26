3edc1095dcc491af6e1413f6913de25b4f2618f2
Markus Holtermann <info@markusholtermann.eu>
2016-02-26 19:34:18 +1100

    Recipe #3 -- Renamed tables to new name

--- new_app_name/models.py
+++ new_app_name/models.py
@@ -4,13 +4,7 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=50)
 
-    class Meta:
-        db_table = 'rename_app_author'
-
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.ForeignKey('new_app_name.Author')
-
-    class Meta:
-        db_table = 'rename_app_book'
