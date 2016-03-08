53b92486ef48e3f91f514d13584922b9db065ca6
Markus Holtermann <info@markusholtermann.eu>
2016-03-08 18:30:53 +1100

    Recipe #2 -- Added new field with null=True

--- non_nullable_field/models.py
+++ non_nullable_field/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=50)
+    homepage = models.URLField(null=True)
 
     @classmethod
     def create(cls, name):
